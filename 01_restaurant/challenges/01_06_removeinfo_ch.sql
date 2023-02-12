3b991441f59392efcdd4ff74acb89a13b238c943
Oleksandr <94463783+Kryvchenko@users.noreply.github.com>
2023-02-12 13:55:08 +0000

    tasks 6,7,8,9 complited

--- 01_restaurant/challenges/01_06_removeinfo_ch.sql
+++ 01_restaurant/challenges/01_06_removeinfo_ch.sql
@@ -3,3 +3,19 @@
 -- A customer named Norby has notified us he won't 
 -- be able to keep his Friday reservation. 
 -- Today is July 24, 2022.
+SELECT *
+FROM Customers
+WHERE FirstName="Norby"
+
+DELETE
+FROM Reservations
+WHERE ReservationID=2000
+
+SELECT *
+FROM Reservations
+WHERE CustomerID=64
+
+--or
+UPDATE Reservations
+SET Date = NULL 
+WHERE ReservationID = 2000;
