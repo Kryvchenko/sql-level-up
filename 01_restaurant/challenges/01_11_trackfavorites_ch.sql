7114c56594c352c2ac334916c39b8bc851c6aedc
Oleksandr <94463783+Kryvchenko@users.noreply.github.com>
2023-02-17 16:30:58 +0000

    first part of the challange completed

--- 01_restaurant/challenges/01_11_trackfavorites_ch.sql
+++ 01_restaurant/challenges/01_11_trackfavorites_ch.sql
@@ -2,3 +2,18 @@
 
 -- Set Cleo Goldwater's favorite dish to
 -- the Quinoa Salmon Salad.
+
+SELECT * 
+FROM Dishes
+WHERE Name = 'Quinoa Salmon Salad';
+
+SELECT * 
+FROM Customers
+WHERE FirstName = 'Cleo' AND LastName = 'Goldwater';
+UPDATE Customers
+SET FavoriteDish = 9
+WHERE CustomerID = 42;
+
+SELECT Customers.FirstName, Customers.LastName, Customers.FavoriteDish, Dishes.DishID 
+FROM Customers
+JOIN DISHES ON Customers.FavoriteDish = Dishes.DishID;
